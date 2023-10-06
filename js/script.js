81022fdd454c40a46a8ccf5a182176a36ec1c5b1
burillu <burillu90@hotmail.it>
2023-10-06 17:36:22 +0200

    add header height, onclick function, navbar, divs

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 const num = 21;
 let firstname, surname, color;
+// input name
 firstname = prompt('Inserisci qui il tuo nome:');
+// input surname
 surname= prompt('Inserisci qua il tuo cognome:')
+// input color
 color= prompt('Qual\'è il tuo colore preferito?');
-let newPsw= (`${firstname}${surname}${color}${num}`)
-console.log(newPsw);
-document.getElementById('psw-container').innerHTML=`<h2>${newPsw}</h2>`;
+//concat newPsw
+let newPsw= (`${firstname.toLowerCase()}${surname.toLowerCase()}${color.toLowerCase()}${num}`);
+
+document.getElementById('firstname').innerHTML=`${firstname}`
+document.getElementById('surname').innerHTML=`${surname}`
+document.getElementById('color').innerHTML=`${color}`
+
+
+function showPsw(){
+    document.getElementById('psw-container').innerHTML=`<h2>${newPsw}</h2>`;
+}
+
